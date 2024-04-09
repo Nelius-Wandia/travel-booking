7d6f0619c1f1ad0e8cafee239534286ac1cb5e24
AndrexCodes <123867051+AndrexCodes@users.noreply.github.com>
2024-04-09 23:26:21 +0300

    Added modules folder for backend

---
+++ TravelEassy Module/admin_users.py
@@ -0,0 +1,18 @@
+class AdminUser:
+    pass
+
+
+# Cols - (id, company_name, phone, email, means_type, till_number, bank_acc, crypto_addr, verification, revenue, total_trips, logo_url, updated_time, created_time)
+
+# FUNCTIONS
+# Rules(Each function takes its own parameters)
+# Rules(Init set the user id)
+# - CreateUser - (Create new user account)
+# - AuthenticateUser - (Allow access based on email/phone and password)
+# - VerifyUser - (Verify user based input verification code)
+# - ControlUser - (Enable/Disable User Account)
+# - UpdateProfile - (Update user profile profile)
+# - DeleteUser - (Delete user account)
+# - IncreamentTrips - (Increament number of trips for the user)
+# - NotifyUser - (Notify user by sms/email address)
+# - SetUserdetails - (Set user details)
