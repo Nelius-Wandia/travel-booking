f5774fd36d1fb4f77a8578082a22b2671edf183d
AndrexCodes <123867051+AndrexCodes@users.noreply.github.com>
2024-04-11 16:33:57 +0300

    Modified db conection module

---
+++ TravelEassy Module/generals.py
@@ -0,0 +1,28 @@
+import random
+import datetime
+from passlib.hash import sha256_crypt
+
+class Generals:
+    def __init__(self):
+        self.alphabets_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.alphabets_lower = self.alphabets_upper.lower()
+        self.numerics = "0123456789"
+        self.symbols = "~!@#$%^&*()_+~"
+        self.combinations = self.alphabets_upper + self.alphabets_lower + self.numerics + self.symbols
+
+    def GenRandomCode(self, size=4):
+        code = ""
+        for _ in range(size):
+            unit_code = random.choice(self.combinations)
+            code += unit_code
+        return code
+    
+    def GetPasswordHash(self, password):
+        return sha256_crypt.hash(password)
+    
+    def VerifyHash(self, password_hash, secret):
+        return sha256_crypt.verify(secret, password_hash)
+    
+    def GetCurrentTime(self):
+        return datetime.datetime.now()
+    
--- TravelEassy Module/main.py
+++ TravelEassy Module/main.py
@@ -1,4 +1,30 @@
+from flask import Flask, jsonify, make_response, request
+from flask_cors import CORS
 from admin_users import AdminUser
 
-x = AdminUser()
-x.CreateUserTable()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    if not request.get_json(): return False
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    user = AdminUser()
+    if not user.AuthenticateUser(email, password):return jsonify({
+        "data": False
+        })
+    print("Passed Auth")
+    # user Authentication using email/phone and password
+    # Auth_token valid for 30 days
+    response = jsonify({
+        "data": "data"
+    })
+    response = make_response(response)
+    response.set_cookie("auth_token","Actual token")
+    return response
+    # Return Access token
+
+if __name__ == '__main__':
+    app.run(debug=True)
