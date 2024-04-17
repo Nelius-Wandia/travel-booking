2ab365df8ba2ed598e2a1866d9122dd9e3304e23
AndrexCodes <123867051+AndrexCodes@users.noreply.github.com>
2024-04-17 21:43:30 +0300

    Modified Update profile

--- TestJS/main.js
+++ TestJS/main.js
@@ -2,8 +2,10 @@ end_point_url = "http://127.0.0.1:5000"
 
 class User {
     // User_id & Access_token are preset within cookies
-    constructor(event) {
-        event.preventDefault()
+    constructor(event = false) {
+        if(event){
+            event.preventDefault()
+        }
         this.url = `${end_point_url}`
     }
 
@@ -40,16 +42,16 @@ class User {
 
         console.log(this.PrepFetch(data))
         fetch(`${this.url}/signup`, this.PrepFetch(data))
-        .then(x => x.json())
-        .then(y => {
-            if (!y["state"]) {
-                console.log(y["message"])
-                // Alert user using the message key to know why signup failed
-            }
-            console.log(y)
-            this.CreateDBStructure()
-            location.href = "home.html"
-        })
+            .then(x => x.json())
+            .then(y => {
+                if (!y["state"]) {
+                    console.log(y["message"])
+                    // Alert user using the message key to know why signup failed
+                }
+                console.log(y)
+                this.CreateDBStructure()
+                location.href = "home.html"
+            })
     }
 
     SignIn(form) {
@@ -66,37 +68,134 @@ class User {
         }
 
         fetch(`${this.url}/signin`, this.PrepFetch(data))
-            .then(x => x.json())
-            .then(y => {
-                if (!y["state"]) {
-                    console.log(y["message"])
-                    if (y["message"] === "Authentication Failed") {
-                        var original_color = form["password"].style.borderColor
-                        form["password"].style.borderColor = "red"
-                        setTimeout(() => {
-                            form["password"].style.borderColor = original_color
-                        }, 1500)
-                    } else {
-                        alert(y["message"])
-                    }
-                    return
-                    // Alert user using the message key to know why signin failed
+        .then(x => x.json())
+        .then(y => {
+            if (!y["state"]) {
+                console.log(y["message"])
+                if (y["message"] === "Authentication Failed") {
+                    var original_color = form["password"].style.borderColor
+                    form["password"].style.borderColor = "red"
+                    setTimeout(() => {
+                        form["password"].style.borderColor = original_color
+                    }, 1500)
+                } else {
+                    alert(y["message"])
                 }
-
-                console.log(y)
-                this.StoreJSONData(y["data"])
-                location.href = "home.html"
-            })
+                return
+                // Alert user using the message key to know why signin failed
+            }
+            console.log(y)
+            this.StoreJSONData(y["data"])
+            location.href = "home.html"
+        })
 
 
     }
 
     UpdateProfile(form) {
-        console.log("testing function")
+        var data = {
+            "company_name": form["company_name"].value,
+            "phone": form["phone"].value,
+            "email": form["email"].value,
+            "till_number": form["till"].value,
+            "bank_account": form["bank"].value,
+            "crypto": form["crypto"].value,
+        }
+        console.log(data)
+        fetch(`${this.url}/account_update`, this.PrepFetch(data))
+        .then(x => x.json())
+        .then(y => {
+            if (!y["state"]) {
+                console.log(y["message"])
+                if (y["message"] === "AuthToken Error") {
+                    location.href = "signin.html"
+                }
+                // Alert user using the message key to know why signin failed
+            }
+            console.log(y)
+            this.StoreJSONData(y["data"])
+            location.href = "profile.html"
+        })
+    }
+
+    RenderJOSNtoDom(parent_element, parent_element_payment) {
+        var account = this.FetchAccountUserData()
+        var textContext =
+            `<div class="row mx-0 mb-3">
+        <div class="col-6 p-0">
+            <small class="text-muted mb-1 f-10 pr-1">Phone</small>
+            <p class="small mb-0 l-hght-14">${account["phone"]}</p>
+        </div>
+        <div class="col-6 p-0">
+            <small class="text-muted mb-1 f-10 pr-1">Email</small>
+            <p class="small mb-0 l-hght-14">${account["email"].substring(0,10)}*****</p>
+        </div>
+    </div>
+    <div class="row mx-0 mb-3">
+        <div class="col-6 p-0">
+            <small class="text-muted mb-1 f-10 pr-1">Means Type</small>
+            <p class="small mb-0 l-hght-14">${account["means_type"]}</p>
+        </div>
+        <div class="col-6 p-0">
+            <small class="text-muted mb-1 f-10 pr-1">Log In Accounts</small>
+            <p class="small mb-0 l-hght-14">${account["log_acc"]}</p>
+        </div>
+    </div>
+    <div class="row mx-0">
+        <div class="col-6 p-0">
+            <small class="text-muted mb-1 f-10 pr-1">Verification</small>
+            <p class="small mb-0 l-hght-14">${account["verification"]}</p>
+        </div>
+        <div class="col-6 p-0">
+            <small class="text-muted mb-1 f-10 pr-1">Status</small>
+            <p class="small mb-0 l-hght-14">${account["status"]}</p>
+        </div>
+    </div>`
+        parent_element.innerHTML = textContext
+        document.getElementById('company_name').innerHTML = account["company_name"]
+
+        console.log("siniwqxoiq")
+        console.log(account)
+        var payment_context = ""
+        if(account["payment"][0]){
+            payment_context += ``
+        }
+
     }
 
     StoreJSONData(json_data) {
         localStorage.setItem("db_data", JSON.stringify(json_data))
+    }
+
+    FetchAccountUserData() {
+        var response = {
+            "company_name": "Invalid",
+            "phone": "Invalid",
+            "email": "Invalid",
+            "means_type": "Invalid",
+            "log_acc": "Invalid",
+            "verification": "Invalid",
+            "status": "Invalid",
+            "payment": []
+        }
+        var db_data = localStorage.getItem("db_data")
+        db_data = JSON.parse(db_data)
+        console.log(db_data)
+        if(!db_data){return response}
+
+        response.company_name = db_data["account_profile"]["company_name"]
+        response.phone = db_data["account_profile"]["phone"]
+        response.email = db_data["account_profile"]["email"]
+        response.means_type = db_data["account_profile"]["means_type"]
+        response.log_acc = 1
+        response.verification = db_data["account_profile"]["verification"]
+        response.status = db_data["account_profile"]["verification"]
+        response.payment = [
+            db_data["account_profile"]["payment_methods"]["BankAccount"],
+            db_data["account_profile"]["payment_methods"]["CryptoAddress"],
+            db_data["account_profile"]["payment_methods"]["Mpesa"]
+        ]
+        return response
     }
 
     PrepFetch(data) {
@@ -111,7 +210,7 @@ class User {
         return options
     }
 
-    CreateDBStructure(){
+    CreateDBStructure() {
         var structure = {
             "account_profile": "",
             "buses": null,
@@ -119,12 +218,25 @@ class User {
         }
         localStorage.setItem("db_data", JSON.stringify(structure))
     }
+
+    EditUserProfile(form){
+        var account_details = this.FetchAccountUserData()
+        console.log(account_details)
+        form["company_name"].value = account_details["company_name"]
+        form["phone"].value = account_details["phone"]
+        form["email"].value = account_details["email"]
+        form["till"].value = account_details["payment"][0]
+        form["bank"].value = account_details["payment"][2]
+        form["crypto"].value = account_details["payment"][1]
+    }
 }
 
 bus_url = "add"
+current_bus_id = ""
+
 class Buses {
-    constructor(event=false) {
-        if(event){
+    constructor(event = false) {
+        if (event) {
             event.preventDefault()
         }
         this.url = `${end_point_url}`
@@ -145,11 +257,30 @@ class Buses {
             "seat_config": seat_config
         }
 
-        if(bus_url === "update"){
+        if (bus_url === "update") {
             this.UpdateBus()
             return
         }
         fetch(`${this.url}/bus/add`, this.PrepFetch(data))
+            .then(x => x.json())
+            .then(y => {
+                console.log(y)
+                if (!y["state"]) {
+                    if (y["message"] === "AuthToken Error") {
+                        location.href = "signin.html"
+                    }
+                }
+                this.StoreJSONBusData(y["data"])
+                this.CloseAddBusForm()
+            })
+    }
+
+    DeleteBus(bus_id) {
+        var data = {
+            "bus_id": bus_id
+        }
+        if (!confirm("Are you sure to delete vehicle ?")) { return }
+        fetch(`${this.url}/bus/delete`, this.PrepFetch(data))
         .then(x => x.json())
         .then(y => {
             console.log(y)
@@ -158,38 +289,21 @@ class Buses {
                     location.href = "signin.html"
                 }
             }
-            this.StoreJSONBusData(y["data"])
-            this.CloseAddBusForm()
-        })
-    }
-
-    DeleteBus(bus_id){
-        var data = {
-            "bus_id": bus_id
-        }
-
-        fetch(`${this.url}/bus/delete`, this.PrepFetch(data))
-        .then(x=>x.json())
-        .then(y=>{
-            console.log(y)
-            if (!y["state"]) {
-                if (y["message"] === "AuthToken Error") {
-                    location.href = "signin.html"
-                }
-            }
+            alert("Vehicle Deleted Successfully")
             this.StoreJSONBusData(y["data"])
         })
     }
 
-    UpdateBus(){
+    UpdateBus() {
         var form = document.forms["bus_form"]
+        var bus_id = current_bus_id
         var licesnse_plate = form["licesnse_plate"].value
         var no_seats = form["no_seats"].value
         var model = form["model"].value
         var colour = form["colour"].value
         var seat_config = form["seat_config"].value
         var data = {
-            "bus_id": "",
+            "bus_id": bus_id,
             "licesnse_plate": licesnse_plate,
             "no_seats": no_seats,
             "model": model,
@@ -197,24 +311,26 @@ class Buses {
             "seat_config": seat_config
         }
         fetch(`${this.url}/bus/update`, this.PrepFetch(data))
-        .then(x=>x.json())
-        .then(y=>{
-            console.log(y)
-            if (!y["state"]) {
-                if (y["message"] === "AuthToken Error") {
-                    location.href = "signin.html"
+            .then(x => x.json())
+            .then(y => {
+                console.log(y)
+                if (!y["state"]) {
+                    if (y["message"] === "AuthToken Error") {
+                        location.href = "signin.html"
+                    }
                 }
-            }
-            this.StoreJSONBusData(y["data"])
-        })
+                this.StoreJSONBusData(y["data"])
+                this.CloseAddBusForm()
+            })
     }
 
-    EditBusdetails(bus_id){
+    EditBusdetails(bus_id) {
         var db_data = localStorage.getItem("db_data")
         db_data = JSON.parse(db_data)
         var all_buses = db_data["buses"]
-        for(var x in all_buses){
-            if(all_buses[x][0] === bus_id){
+        for (var x in all_buses) {
+            if (all_buses[x][0] === bus_id) {
+                current_bus_id = bus_id
                 this.OpenAddBusForm(all_buses[x])
             }
         }
@@ -284,7 +400,7 @@ class Buses {
                             <span class="icofont-google-map small mr-2 text-danger"></span>
                             <div>
                                 <small class="text-muted mb-2 d-block">Status</small>
-                                <p class="small mb-1">${all_buses[bus][8] === 1 ? "BOOKED": "UNBOOKED"}</p>
+                                <p class="small mb-1">${all_buses[bus][8] === 1 ? "BOOKED" : "UNBOOKED"}</p>
                             </div>
                         </div>
                         <button class="col-5 p-2 rounded-2 bg-danger text-white ring-0 mt-2" onclick="new Buses().EditBusdetails('${all_buses[bus][0]}')">Edit</button>
@@ -293,14 +409,14 @@ class Buses {
                 </div>
             </div>
                 </div>`
-            rendered_buses +=unit_bus
+            rendered_buses += unit_bus
         }
-        
+
         parent_element.innerHTML = ""
         parent_element.innerHTML = rendered_buses
     }
 
-    StoreJSONBusData(bus_data){
+    StoreJSONBusData(bus_data) {
         var db_data = localStorage.getItem('db_data')
         db_data = JSON.parse(db_data)
         db_data["buses"] = bus_data
@@ -308,11 +424,11 @@ class Buses {
         new Buses().RenderJSONtoDOM(document.getElementById('all_buses_display'))
     }
 
-    CloseAddBusForm(){
+    CloseAddBusForm() {
         document.getElementById('new-bus-form').style.display = 'none'
     }
 
-    OpenAddBusForm(bus_data){
+    OpenAddBusForm(bus_data) {
         this.bus_id = bus_data[0]
         var bus_form = document.forms["bus_form"]
         console.log(bus_form)
@@ -325,3 +441,4 @@ class Buses {
         document.getElementById('new-bus-form').style.display = 'block'
     }
 }
+
